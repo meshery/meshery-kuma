4a2da1245553ae522bc8ebd6ff4681723b5e8fd3
ashish <ashishjaitiwari15112000@gmail.com>
2021-12-20 11:46:40 +0530

    Use new meshkit function
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- internal/config/operations.go
+++ internal/config/operations.go
@@ -13,7 +13,7 @@ var (
 func getOperations(dev adapter.Operations) adapter.Operations {
 
 	var adapterVersions []adapter.Version
-	versions, _ := utils.GetLatestReleaseTagsSorted("istio", "istio")
+	versions, _ := utils.GetLatestReleaseTagsSorted("kumahq", "kuma")
 	for _, v := range versions {
 		adapterVersions = append(adapterVersions, adapter.Version(v))
 	}
