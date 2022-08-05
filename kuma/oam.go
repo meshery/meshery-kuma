63599c5486a7e1dc515110058e6f6269e48516b7
Ruturaj Mohite <mohite.ruturaj15@gmail.com>
2022-08-05 19:39:06 +0530

    Handle error
    Signed-off-by: Ruturaj Mohite <mohite.ruturaj15@gmail.com>

--- kuma/oam.go
+++ kuma/oam.go
@@ -90,6 +90,10 @@ func handleComponentKumaMesh(kuma *Kuma, comp v1alpha1.Component, isDel bool, ku
 	// we are sure that the version of kuma would be present
 	// because the configuration is already validated against the schema
 	version := comp.Spec.Version
+	if version == "" {
+		return "", fmt.Errorf("pass valid version inside service for Istio installation")
+	}
+	//TODO: When no version is passed in service, use the latest Kuma version
 
 	msg, err := kuma.installKuma(isDel, false, comp.Namespace, version, kubeconfigs)
 	if err != nil {
