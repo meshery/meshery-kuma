12064e6946ba7e2a65fb84a24f0e092c7d97ddd8
Ruturaj Mohite <mohite.ruturaj15@gmail.com>
2022-12-04 14:46:27 +0530

    Add meshmodel support
    Signed-off-by: Ruturaj Mohite <mohite.ruturaj15@gmail.com>

--- kuma/oam/register.go
+++ kuma/oam/register.go
@@ -4,28 +4,58 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/layer5io/meshery-adapter-library/adapter"
 	"github.com/layer5io/meshery-kuma/internal/config"
+	"github.com/layer5io/meshkit/models/meshmodel/core/types"
 )
 
 var (
 	basePath, _ = os.Getwd()
 
 	//WorkloadPath will be used by both static and component generation
-	WorkloadPath     = filepath.Join(basePath, "templates", "oam", "workloads")
-	traitPath        = filepath.Join(basePath, "templates", "oam", "traits")
-	workloadPathSets = []schemaDefinitionPathSet{}
+	WorkloadPath        = filepath.Join(basePath, "templates", "oam", "workloads")
+	MeshmodelComponents = filepath.Join(basePath, "templates", "meshmodel", "components")
+	traitPath           = filepath.Join(basePath, "templates", "oam", "traits")
+	workloadPathSets    = []schemaDefinitionPathSet{}
 )
 
 // AvailableVersions denote the component versions available statically
 var AvailableVersions = map[string]bool{}
+var availableVersionGlobalMutex sync.Mutex
 
 type schemaDefinitionPathSet struct {
 	oamDefinitionPath string
 	jsonSchemaPath    string
 	name              string
+}
+
+type meshmodelDefinitionPathSet struct {
+	meshmodelDefinitionPath string
+}
+
+func RegisterMeshModelComponents(uuid, runtime, host, port string) error {
+	meshmodelRDP := []adapter.MeshModelRegistrantDefinitionPath{}
+	pathSets, err := loadMeshmodelComponents(MeshmodelComponents)
+	if err != nil {
+		return err
+	}
+	portint, _ := strconv.Atoi(port)
+	for _, pathSet := range pathSets {
+		meshmodelRDP = append(meshmodelRDP, adapter.MeshModelRegistrantDefinitionPath{
+			EntityDefintionPath: pathSet.meshmodelDefinitionPath,
+			Host:                host,
+			Port:                portint,
+			Type:                types.ComponentDefinition,
+		})
+	}
+
+	return adapter.
+		NewMeshModelRegistrant(meshmodelRDP, fmt.Sprintf("%s/api/meshmodel/components/register", runtime)).
+		Register(uuid)
 }
 
 // RegisterWorkloads will register all of the workload definitions
@@ -85,6 +115,31 @@ func RegisterTraits(runtime, host string) error {
 		Register()
 }
 
+func loadMeshmodelComponents(basepath string) ([]meshmodelDefinitionPathSet, error) {
+	res := []meshmodelDefinitionPathSet{}
+	if err := filepath.Walk(basepath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		res = append(res, meshmodelDefinitionPathSet{
+			meshmodelDefinitionPath: path,
+		})
+		availableVersionGlobalMutex.Lock()
+		AvailableVersions[filepath.Base(filepath.Dir(path))] = true // Getting available versions already existing on file system
+		availableVersionGlobalMutex.Unlock()
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
 func load(basePath string) ([]schemaDefinitionPathSet, error) {
 	res := []schemaDefinitionPathSet{}
 
@@ -107,7 +162,9 @@ func load(basePath string) ([]schemaDefinitionPathSet, error) {
 				jsonSchemaPath:    fmt.Sprintf("%s.meshery.layer5io.schema.json", nameWithPath),
 				name:              filepath.Base(nameWithPath),
 			})
+			availableVersionGlobalMutex.Lock()
 			AvailableVersions[filepath.Base(filepath.Dir(path))] = true
+			availableVersionGlobalMutex.Unlock()
 		}
 
 		return nil
